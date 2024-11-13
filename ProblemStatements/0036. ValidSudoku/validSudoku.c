f9be5093a7455174bab1820d60b7876e84bdbcc2
Roonil03 <aryanvivek05@gmail.com>
2024-11-13 08:21:30 +0530

    solved problem 36

---
+++ ProblemStatements/0036. ValidSudoku/validSudoku.c
@@ -0,0 +1,23 @@
+#include <stdbool.h>
+
+bool isValidSudoku(char** board, int boardSize, int* boardColSize) {
+    bool row[9][9] = {false};
+    bool col[9][9] = {false};
+    bool box[9][9] = {false};
+     for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            if (board[i][j] != '.') {
+                int num = board[i][j] - '1';
+                int boxIndex = (i / 3) * 3 + (j / 3);
+                if (row[i][num] || col[j][num] || box[boxIndex][num]) {
+                    return false;
+                }
+
+                row[i][num] = true;
+                col[j][num] = true;
+                box[boxIndex][num] = true;
+            }
+        }
+    }
+    return true;
+}
