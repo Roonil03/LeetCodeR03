eeb707170709900328079f80d521cc5f2b74893d
Roonil03 <aryanvivek05@gmail.com>
2024-12-29 18:19:35 +0530

    solved problem 3402

---
+++ ProblemStatements/3402. MinimumOperationsToMakeColumnsStrictlyIncreasing/minOpsForCols.go
@@ -0,0 +1,14 @@
+func minimumOperations(grid [][]int) int {
+	m, n := len(grid), len(grid[0])
+	ops := 0
+	for j := 0; j < n; j++ {
+		for i := 1; i < m; i++ {
+			if grid[i][j] <= grid[i-1][j] {
+				ops += grid[i-1][j] - grid[i][j] + 1
+				grid[i][j] = grid[i-1][j] + 1
+			}
+		}
+	}
+	return ops
+
+}
