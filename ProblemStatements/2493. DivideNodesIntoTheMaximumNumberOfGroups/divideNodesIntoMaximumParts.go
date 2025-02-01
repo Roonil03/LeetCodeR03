55b8fc092f0c747a2de2f5eaa20925a10b095a74
Roonil03 <aryanvivek05@gmail.com>
2025-02-01 09:29:38 +0530

    solved problem 2493

---
+++ ProblemStatements/2493. DivideNodesIntoTheMaximumNumberOfGroups/divideNodesIntoMaximumParts.go
@@ -0,0 +1,52 @@
+func magnificentSets(n int, edges [][]int) int {
+	g := make([][]int, n)
+	for i := range g {
+		g[i] = make([]int, 0)
+	}
+	for _, e := range edges {
+		a, b := e[0]-1, e[1]-1
+		g[a] = append(g[a], b)
+		g[b] = append(g[b], a)
+	}
+	d := make([]int, n)
+	for i := 0; i < n; i++ {
+		q := []int{i}
+		dist := make([]int, n)
+		dist[i] = 1
+		mx := 1
+		root := i
+		for len(q) > 0 {
+			a := q[0]
+			q = q[1:]
+			if a < root {
+				root = a
+			}
+			for _, b := range g[a] {
+				if dist[b] == 0 {
+					dist[b] = dist[a] + 1
+					if dist[b] > mx {
+						mx = dist[b]
+					}
+					q = append(q, b)
+				} else if abs(dist[b]-dist[a]) != 1 {
+					return -1
+				}
+			}
+		}
+		if mx > d[root] {
+			d[root] = mx
+		}
+	}
+	sum := 0
+	for _, val := range d {
+		sum += val
+	}
+	return sum
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
