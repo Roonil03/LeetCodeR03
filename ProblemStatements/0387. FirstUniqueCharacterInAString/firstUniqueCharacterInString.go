8ada7a83da2c8434eec332fa985ab71ebbeba0f9
Roonil03 <aryanvivek05@gmail.com>
2025-09-01 19:21:22 +0530

    solved problem 387

---
+++ ProblemStatements/0387. FirstUniqueCharacterInAString/firstUniqueCharacterInString.go
@@ -0,0 +1,12 @@
+func firstUniqChar(s string) int {
+	freq := make(map[rune]int)
+	for _, ch := range s {
+		freq[ch]++
+	}
+	for i, ch := range s {
+		if freq[ch] == 1 {
+			return i
+		}
+	}
+	return -1
+}
