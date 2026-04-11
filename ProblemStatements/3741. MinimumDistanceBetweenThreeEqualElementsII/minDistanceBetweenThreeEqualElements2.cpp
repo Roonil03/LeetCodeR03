7e392956777e1d82ccc411c5d3813aede68c78ef
Roonil03 <aryanvivek05@gmail.com>
2026-04-11 10:59:43 +0530

    solved problem 3741

---
+++ ProblemStatements/3741. MinimumDistanceBetweenThreeEqualElementsII/minDistanceBetweenThreeEqualElements2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    static const int N=1e5+1;
+    int pos[N][2]={[0 ... N-1][0 ... 1]=-1};
+
+    int minimumDistance(vector<int>& nums) {
+        int n=nums.size();
+        int ans=INT_MAX, M=0;
+        for(int k=0; k<n; k++){
+            const int x=nums[k];
+            M=max(M, x);
+            if (pos[x][1]!=-1){
+                ans=min(ans, (k-pos[x][1])<<1);
+            }
+            pos[x][1]=exchange(pos[x][0], k);
+        }
+        memset(pos, -1, sizeof(int)*2*(M+1));
+        return ans==INT_MAX?-1:ans;
+    }
+};
