04abd390035f1e33a0bbe9671f09ab8e241ee8fb
Roonil03 <aryanvivek05@gmail.com>
2026-04-13 10:50:50 +0530

    solved problem 1848

---
+++ ProblemStatements/1848. MinimumDistanceToTheTargetElement/minDistanceToTargetElement.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        int n = nums.size();
+        for(int i {0}; i < n; i++){
+            if(start - i >= 0 && nums[start - i] == target){
+                return i;
+            }
+            if(start + i < n && nums[start + i] == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
