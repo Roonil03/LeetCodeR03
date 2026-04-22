15c84a3556e2ca90866208acd183d220550290fe
Roonil03 <aryanvivek05@gmail.com>
2026-04-22 05:48:14 +0530

    solved problem 2452

---
+++ ProblemStatements/2452. WordsWithinTwoEditsOfDictionary/wordsWithinTwoEditsDict.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        vector<string> res;
+        for(auto& q: queries){
+            for(auto& i: dictionary){
+                if(inner_product(begin(q), end(q), begin(i), 0, plus<int>(), not_equal_to<char>()) < 3){
+                    res.push_back(q);
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
