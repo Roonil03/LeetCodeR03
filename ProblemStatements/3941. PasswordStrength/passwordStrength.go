8d80ba573c9b19cd08e9e542fe7483f144c47702
Roonil03 <aryanvivek05@gmail.com>
2026-06-02 09:30:12 +0530

    solved problem 3941

---
+++ ProblemStatements/3941. PasswordStrength/passwordStrength.go
@@ -0,0 +1,22 @@
+func passwordStrength(password string) int {
+    var vis [256]bool
+    res := 0
+    for i := range password{
+        b := password[i]
+        if vis[b]{
+            continue
+        }
+        vis[b] = true
+        switch{
+            case b >= 'a' && b <= 'z':
+                res += 1
+            case b >= 'A' && b <= 'Z':
+                res += 2
+            case b >= '0' && b <= '9':
+                res += 3
+            case b == '!' || b == '@' || b == '#' || b == '$':
+                res += 5
+        }
+    }
+    return res
+}
