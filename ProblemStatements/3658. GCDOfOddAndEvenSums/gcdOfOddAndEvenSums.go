70b5ab063092f7c570687b6ea1de20ccba3f9576
Roonil03 <aryanvivek05@gmail.com>
2026-07-15 11:50:03 +0530

    solved problem 3658

---
+++ ProblemStatements/3658. GCDOfOddAndEvenSums/gcdOfOddAndEvenSums.go
@@ -0,0 +1,13 @@
+func gcdOfOddEvenSums(n int) int {
+    return n
+}
+
+// func gcd(a, b int) int{
+//     for b != 0 {
+// 		a, b = b, a%b
+// 	}
+// 	if a < 0 {
+// 		return -a
+// 	}
+// 	return a
+// }
