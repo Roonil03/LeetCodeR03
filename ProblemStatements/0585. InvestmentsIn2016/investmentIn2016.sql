628c54d68383f7a5669f04dc99c4d492784e0be9
Roonil03 <aryanvivek05@gmail.com>
2026-09-20 19:25:22 +0530

    solved problem 585

---
+++ ProblemStatements/0585. InvestmentsIn2016/investmentIn2016.sql
@@ -0,0 +1,2 @@
+-- Write your PostgreSQL query statement below
+SELECT ROUND(SUM(tiv_2016)::numeric, 2) AS tiv_2016 FROM (SELECT tiv_2016, COUNT(*) OVER (PARTITION BY tiv_2015) AS c1, COUNT(*) OVER(PARTITION BY lat, lon) as c2 FROM Insurance) t WHERE c1 > 1 AND c2 = 1;
