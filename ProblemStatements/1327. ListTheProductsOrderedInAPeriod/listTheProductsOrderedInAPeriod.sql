6c0792409b6333fb3deeb94efc5bd0de707a933d
Roonil03 <aryanvivek05@gmail.com>
2026-09-21 05:00:03 +0530

    solved problem 1327

---
+++ ProblemStatements/1327. ListTheProductsOrderedInAPeriod/listTheProductsOrderedInAPeriod.sql
@@ -0,0 +1,2 @@
+-- Write your PostgreSQL query statement below
+SELECT p.product_name, SUM(o.unit) AS unit FROM Products p JOIN Orders o USING (product_id) WHERE order_date BETWEEN '2020-02-01' AND '2020-02-29' GROUP BY p.product_id, p.product_name HAVING SUM(o.unit) >= 100;
